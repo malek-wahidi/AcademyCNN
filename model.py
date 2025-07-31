cfb01933f1a781f06a65d4c09f3d712244ede680
Charbel_Jbeily <charbeljbeily17@gmail.com>
2025-07-31 07:58:07 +0300

    Increased to 40 epochs and added a third convolutional layer

--- model.py
+++ model.py
@@ -17,14 +17,22 @@ class ImprovedNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
         self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(64 * 8 * 8, 256)
+        self.fc1 = nn.Linear(128 * 8 * 8, 256)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
         x = rearrange(x, 'b c h w -> b (c h w)')
         x = self.dropout(F.relu(self.fc1(x)))
         x = F.relu(self.fc2(x))
