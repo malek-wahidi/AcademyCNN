1d41224550c9a88d94ad28a357bb0fc3e4e84c9a
Charbel_Jbeily <charbeljbeily17@gmail.com>
2025-07-31 10:06:12 +0300

    Added data augmentation / Adjusted the lr and number of epochs again

--- train.py
+++ train.py
@@ -15,10 +15,16 @@ from model import ImprovedNet
 with open('config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 
-# Standard input normalization for CIFAR-10
-transform = transforms.Compose([
+transform_train = transforms.Compose([
+    transforms.RandomHorizontalFlip(),    
+    transforms.RandomCrop(32, padding=4),         
     transforms.ToTensor(), # Convert PIL image to PyTorch Tensor and normalize to [0, 1]
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # Mean and Standard Deviation of the CIFAR-10 dataset
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 def get_loaders():
@@ -31,13 +37,13 @@ def get_loaders():
         root=config['paths']['train_dir'],
         train=True,
         download=True,
-        transform=transform
+        transform=transform_train
     )
     dataset_test = datasets.CIFAR10(
         root=config['paths']['test_dir'],
         train=False,
         download=True,
-        transform=transform
+        transform=transform_test
     )
 
     # Split train into train/val (e.g., 90% train, 10% val)
